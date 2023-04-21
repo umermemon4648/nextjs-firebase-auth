7d6032e5b918665b729ee6a3c502ac79cb15448d
umermemon4648 <umermemon4648@gmail.com>
2023-04-22 00:49:51 +0500

    implemented GitHub and Facebook authentication logic

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -1,5 +1,5 @@
 import {auth, fbProvider, googleProvider, githubProvider} from '../../config/firebaseConfig'
-import {signInWithPopup, signOut} from 'firebase/auth'
+import {FacebookAuthProvider, signInWithPopup, signOut, updateProfile} from 'firebase/auth'
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import {AiFillFacebook,AiFillGithub} from 'react-icons/ai'
@@ -9,8 +9,8 @@ const signin = () => {
   const router = useRouter();
     const [user, setUser] = useState(null);
     const [saveuser, setSavedUser] = useState(null);
-
-
+    const [profilePicture, setProfilePicture] = useState(null);
+// sign in with Google
     const googleSignin = async () => {
       try {
         const result = await signInWithPopup(auth, googleProvider);
@@ -30,19 +30,58 @@ const signin = () => {
     
 
 
-  //   const signOutHandler =  async()=>{
-  //     await signOut(auth)
-  //     let authenticatedUser = null
-  //     setUser(authenticatedUser);
-  //     localUser = localStorage.clear()
-  //     alert("Signout SuccessFully")
-  // }
+    // sign in with Facebook
+    const fbSignin = async () => {
+      try {
+        const result = await signInWithPopup(auth, fbProvider);
+        const authenticatedUser = result?.user;
+        if (authenticatedUser) {
+          console.log(authenticatedUser);
+          setUser(localStorage.setItem('user', JSON.stringify(authenticatedUser)))
+          router.push('/auth/success');
+        }
+        const credential =  FacebookAuthProvider.credentialFromResult(result)
+        console.log("credentials :",credential);
+        const accessToken = credential.accessToken
+        console.log("accessToken: ",accessToken);
+        fetch(`https://graph.facebook.com/${result.user.providerData[0].uid}/picture?type=large&access_token=${accessToken}`)
+        .then((response)=>response.blob())
+        .then((blob)=>{
+          setProfilePicture(URL.createObjectURL(blob));
+        })
+        // let photoURL = result.user.photoURL+ '?height=500&access_token='+token
+        // await updateProfile(auth.currentUser, {photoURL: photoURL})
+      
+        
+      } catch (error) {
+        console.log("Facebook Signin Error: ", error);
+      }
+    };
+
+
+        // sign in with Github
+        const githubSignin = async () => {
+          try {
+            const result = await signInWithPopup(auth, githubProvider);
+            const authenticatedUser = result?.user;
+            if (authenticatedUser) {
+              console.log(authenticatedUser)
+              setUser(localStorage.setItem('user', JSON.stringify(authenticatedUser)))
+              router.push('/auth/success');
+            }
+          } catch (error) {
+            console.log("Github Signin Error: ", error);
+          }
+        };
+    
+
 
     useEffect(()=>{
       const storedUser = localStorage.getItem('user');
       if (storedUser) {
         setSavedUser(JSON.parse(storedUser));
       }
+
     },[])
 
 
@@ -65,11 +104,11 @@ const signin = () => {
     <FcGoogle className='text-2xl'/>
     Continue with Google</button>
   
-  <button className='text-white bg-gray-800 w-1/2 p-3 font-medium rounded-lg flex align-middle gap-2 hover:bg-gray-700' onClick={googleSignin} type="submit">
+  <button className='text-white bg-gray-800 w-1/2 p-3 font-medium rounded-lg flex align-middle gap-2 hover:bg-gray-700' onClick={fbSignin} type="submit">
     <AiFillFacebook className='text-2xl text-blue-400'/>
     Continue with Facebook</button>
 
-    <button className='text-white bg-gray-800 w-1/2 p-3 font-medium rounded-lg flex align-middle gap-2 hover:bg-gray-700' onClick={googleSignin} type="submit">
+    <button className='text-white bg-gray-800 w-1/2 p-3 font-medium rounded-lg flex align-middle gap-2 hover:bg-gray-700' onClick={githubSignin} type="submit">
     <AiFillGithub className='text-2xl '/>
     Continue with Github</button>
 
