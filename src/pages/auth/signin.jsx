8dca89aef8db1be5277bd165c3b8d23570e3c1c6
umermemon4648 <umermemon4648@gmail.com>
2023-04-21 19:47:53 +0500

    completed the feature of  Google auth

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -8,6 +8,7 @@ import {FcGoogle} from 'react-icons/fc'
 const signin = () => {
   const router = useRouter();
     const [user, setUser] = useState(null);
+    const [saveuser, setSavedUser] = useState(null);
 
 
     const googleSignin = async () => {
@@ -15,9 +16,8 @@ const signin = () => {
         const result = await signInWithPopup(auth, googleProvider);
         const authenticatedUser = result?.user;
         if (authenticatedUser) {
-          setUser(authenticatedUser);
+          setUser(localStorage.setItem('user', JSON.stringify(authenticatedUser)))
           router.push('/auth/success');
-          localStorage.setItem('user', JSON.stringify(authenticatedUser))
           console.log("user: ", authenticatedUser);
           console.log("email: ", authenticatedUser.email);
           console.log("displayName: ", authenticatedUser.displayName);
@@ -39,23 +39,24 @@ const signin = () => {
   // }
 
     useEffect(()=>{
-      // googleSignin()
-      // signOutHandler()
-      console.log('useeffect: ', user)
+      const storedUser = localStorage.getItem('user');
+      if (storedUser) {
+        setSavedUser(JSON.parse(storedUser));
+      }
     },[])
 
 
   return (
     <>
-    
-    {/* <div>signin</div> */}
+  
+  {saveuser? 
+(
+  <button className='mt-4 mx-auto text-white bg-gray-800 w-[12rem] p-3 font-medium rounded-lg flex align-middle gap-2 hover:bg-gray-700' onClick={()=> router.push('/auth/success')} type="submit">Go to the Dashboard</button>
 
-    {/* {localUser ? (
-  <button onClick={signOutHandler} type="submit">Sign Out</button>
-) : (
-  <button onClick={googleSignin} type="submit">Sign in with Google</button>
-)} */}
-<div className='w-2/5 mx-auto  shadow-xl mt-32 bg-[#fafafa] p-10 text-gray-700 rounded-lg '>
+):
+(
+  <>
+  <div className='w-2/5 mx-auto  shadow-xl mt-32 bg-[#fafafa] p-10 text-gray-700 rounded-lg border-2 '>
 
 <h1 className='text-red-400 text-3xl font-bold underline'>Register your account </h1>
 
@@ -77,8 +78,17 @@ const signin = () => {
   <div>
     <p className='mt-3 text-gray-700'>By joining, you agree to our <strong>Terms of Service</strong>  and <strong>Privacy Policy</strong></p>
   </div>
-
 </div>
+  </>
+)  
+  
+}
+
+
+
+
+
+
     </>
   )
 }
--- src/pages/auth/success.jsx
+++ src/pages/auth/success.jsx
@@ -11,20 +11,39 @@ const success = () => {
 
     const signOutHandler =  async()=>{
         await signOut(auth)
-        // console.log("result: ",result)
-        let authenticatedUser = null
-        setUser(authenticatedUser);
         localStorage.clear()
         router.push('/auth/signin')
         alert("Signout SuccessFully")
         
     }
 
+    useEffect(()=>{
+      const storedUser = localStorage.getItem('user');
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    },[])
+
   return (
     <>
     <h1>Firebase Signin Successfully</h1>
 
-    <button onClick={signOutHandler} type="submit">Sign Out</button>
+    <button className='mt-4 mx-auto text-white bg-gray-800 w-[10rem] p-3 font-medium rounded-lg flex align-middle gap-2 hover:bg-gray-700' onClick={signOutHandler} type="submit">Sign Out</button>
+
+{user? (
+<>
+<h1>Email: {user.email}</h1>
+<h1>Name: {user.displayName}</h1>
+<img src={user.photoURL} width="100px" height="100px" alt="AVATAR"  />
+</>
+):
+ (
+<>
+<h1>No User Found</h1>
+</>
+ )
+
+}
     
     
     </>
